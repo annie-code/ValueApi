9da8e8322bece0ac048810f7b9c982c3be26f027
Annie <annarohanbabu@gmail.com>
2021-08-08 16:12:59 +0530

    second commit

--- ValueService/Controllers/WeatherForecastController.cs
+++ ValueService/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace ValueService.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "veryCold", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
